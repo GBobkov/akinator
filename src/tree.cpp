672cd98685d48f17b049f80ea1f4cc615f8b0d91
GBobkov <bobkoveu@gmail.com>
2024-11-16 13:02:13 +0300

    удобнее dump

--- src/tree.cpp
+++ src/tree.cpp
@@ -48,7 +48,10 @@ static void Nodes_Init_2Dump(FILE* dump_file, NODE* node)
     assert(node != NULL);
 
     #define TITLE_COLOR "\"lightblue\""
-    fprintf (dump_file, "NODE_0x%p[label = \"{ <name>name = N_%p|<d>data = %s|<p>parent = %p|<l>left = %p|<r>right = %p }\", fillcolor =" TITLE_COLOR "];\n", node, node, node->data, node->parent, node->left, node->right);
+    if (node->left)
+        fprintf (dump_file, "NODE_0x%p[label = \"%s?\", fillcolor =" TITLE_COLOR "];\n", node, node->data);
+    else 
+        fprintf (dump_file, "NODE_0x%p[label = \"%s\", fillcolor =" TITLE_COLOR "];\n", node, node->data);
     if (node->left) Nodes_Init_2Dump(dump_file, node->left);
     if (node->right) Nodes_Init_2Dump(dump_file, node->right);
     #undef TITLE_COLOR
@@ -63,12 +66,12 @@ static void Write_Connections_2Dump(FILE* dump_file, NODE* node)
 
     if (node->left)
     {
-        fprintf(dump_file, "NODE_0x%p->NODE_0x%p [weight = 0, color = deeppink]\n", node, node->left);
+        fprintf(dump_file, "NODE_0x%p->NODE_0x%p [label = \"Нет\", weight = 0, color = deeppink]\n", node, node->left);
         Write_Connections_2Dump(dump_file, node->left);
     }
     if (node->right)
     {
-        fprintf(dump_file, "NODE_0x%p->NODE_0x%p [weight = 0, color = deeppink]\n", node, node->right);
+        fprintf(dump_file, "NODE_0x%p->NODE_0x%p [label = \"Да\", weight = 0, color = deeppink]\n", node, node->right);
         Write_Connections_2Dump(dump_file, node->right);
     }
 }
@@ -90,9 +93,9 @@ void Node_Dump(const char* dump_fname, NODE* node)
 
     fprintf (dump_file, "digraph G\n");
     fprintf (dump_file, "{\n");
-    fprintf (dump_file, "splines=curved;\n");
+    fprintf (dump_file, "splines=line;\n");
     fprintf (dump_file, "nodesep=0.5;\n"); // расстояние между ячейками
-    fprintf (dump_file, "node[shape=\"record\", style=\"rounded, filled\"];\n\n");
+    fprintf (dump_file, "node[shape=\"oval\", style=\"rounded, filled\"];\n\n");
 
     Nodes_Init_2Dump(dump_file, node);
     fprintf (dump_file, "\n");
